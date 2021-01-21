9d311bb78f31a6ff7851c6109f5e9f296a9c3fa4
Otarsots <tom.starost@tu-ilmenau.de>
2021-01-21 19:22:34 +0100

    test

---
+++ campus.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="campus" tilewidth="320" tileheight="320" tilecount="2" columns="2">
+ <image source="../../Pictures/campus.png" width="868" height="626"/>
+</tileset>
